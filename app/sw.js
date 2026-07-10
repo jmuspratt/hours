00840df6c00267e052b5e4f8116e2fec5d135f68
James Muspratt <jmuspratt@users.noreply.github.com>
2026-07-09 21:25:14 -0400

    Tweaks

--- app/sw.js
+++ app/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'hours-20260709113357';
+const CACHE = 'hours-20260709212330';
 const APP_SHELL = ["/", "/index.html", "/style.css", "/app.js", "/sw.js"];
 
 self.addEventListener("install", (e) => {
