a632dd10157464bc94daa2149504d3781b512170
James Muspratt <jmuspratt@users.noreply.github.com>
2026-07-09 21:26:35 -0400

    Deploy

--- app/sw.js
+++ app/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'hours-20260709212330';
+const CACHE = 'hours-20260709212526';
 const APP_SHELL = ["/", "/index.html", "/style.css", "/app.js", "/sw.js"];
 
 self.addEventListener("install", (e) => {
