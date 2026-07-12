d76e5781588d35391c387d28aefc0d5d807570f6
James Muspratt <jmuspratt@users.noreply.github.com>
2026-07-12 08:42:46 -0400

    Shrink icon

--- app/sw.js
+++ app/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'hours-20260709212526';
+const CACHE = 'hours-20260712083824';
 const APP_SHELL = ["/", "/index.html", "/style.css", "/app.js", "/sw.js"];
 
 self.addEventListener("install", (e) => {
